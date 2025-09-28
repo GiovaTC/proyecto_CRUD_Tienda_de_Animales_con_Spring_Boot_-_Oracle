e53d1ab7c4cf188bab84993d4f1b306d898bf9bd
gtapiero <giovanny.tapiero@pi.edu.co>
2025-09-28 09:51:19 -0500

    commit numero tres

--- src/main/java/com/example/petshoporacle/model/Pet.java
+++ src/main/java/com/example/petshoporacle/model/Pet.java
@@ -12,4 +12,96 @@ public class Pet {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "pet_seq")
     private Long id;
+
+    @NotBlank(message = "el nombre es obligatorio")
+    @Column(nullable = false)
+    private String name;
+
+    private String species;
+    private String breed;
+
+    @Min(0)
+    private Integer age;
+    private Double price;
+
+    @Column(length = 2000)
+    private String description;
+
+    private LocalDate birthDate;
+    private String imageUrl;
+
+    public Pet() {}
+
+    // getters y setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 }

80995d12ff92d5cab48cb86204e0c785f0421580
gtapiero <giovanny.tapiero@pi.edu.co>
2025-09-28 09:06:33 -0500

    commit inicial

---
+++ src/main/java/com/example/petshoporacle/PetshopOracleApplication.java
@@ -0,0 +1,13 @@
+package com.example.petshoporacle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetshopOracleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetshopOracleApplication.class, args);
+    }
+
+}
